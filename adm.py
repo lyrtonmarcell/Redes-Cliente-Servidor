b7f5a87d52035197981fce6aad44faf887c2ba80
lyrtonmarcell <104734284+lyrtonmarcell@users.noreply.github.com>
2023-09-03 17:25:19 -0300

    Update adm.py

--- adm.py
+++ adm.py
@@ -1,60 +1,64 @@
-import socket
+import http.client
 import json
-def get_clients():
-# Cria um socket para a comunicação com o servidor
-    client_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-    # Conecta-se ao endereço IP e porta do servidor
-    client_socket.connect(('127.0.0.1', 8080))
-    # Cria a requisição GET com os cabeçalhos necessários
-    request = "GET /get_clients HTTP/1.1\r\nHost: 127.0.0.1:8080\r\n\r\n"
-    # Envia a requisição para o servidor
-    client_socket.send(request.encode('utf-8'))
-    # Inicializa uma variável para armazenar a resposta do servidor
-    response_data = b""
-    # Loop para receber os dados da resposta em blocos de 1024 bytes
+
+def send_get_request(host, path):
+    conn = http.client.HTTPConnection(host)
+    conn.request("GET", path)
+    response = conn.getresponse()
+    data = response.read().decode()
+    conn.close()
+    return response.status, data
+
+def send_put_request(host, path, payload):
+    headers = {"Content-Type": "application/json"}
+    payload_json = json.dumps(payload)
+    conn = http.client.HTTPConnection(host)
+    conn.request("PUT", path, body=payload_json, headers=headers)
+    response = conn.getresponse()
+    data = response.read().decode()
+    conn.close()
+    return response.status, data
+
+def main():
+    host = "127.0.0.1:3000"  # Endereço do servidor RESTful
+
     while True:
-        data = client_socket.recv(1024)
-        if not data:
+        print("Escolha uma opção:")
+        print("1. Ver informações das caixas (GET)")
+        print("2. Modificar informações de uma caixa (PUT)")
+        print("3. Ver informações dos históricos de compras (GET)")
+        print("4. Sair")
+        choice = input("Opção: ")
+
+        if choice == "1":
+            status, response = send_get_request(host, "/caixas")
+            if status == 200:
+                caixas = json.loads(response)
+                for nome_caixa, detalhes_caixa in caixas.items():
+                    print(f"Nome: {nome_caixa}, Endereço: {detalhes_caixa['endereco']}, Status: {detalhes_caixa['status']}")
+            else:
+                print("Falha ao obter informações das caixas.")
+        elif choice == "2":
+            caixa = input("Digite o nome da caixa: ")
+            status = input("Digite o novo status (Aberto ou Bloqueado): ")
+            payload = {"status": status}
+            status, response = send_put_request(host, f"/caixas/{caixa}", payload)
+            if status == 200:
+                print(json.loads(response)["message"])
+            else:
+                print("Falha ao atualizar o status da caixa.")
+        elif choice == "3":
+            status, response = send_get_request(host, "/tags")
+            if status == 200:
+                tags = json.loads(response)
+                formatted_tags = json.dumps(tags, indent=2)
+                print(formatted_tags)
+            else:
+                print("Falha ao obter informações de tags.")
+        elif choice == "4":
             break
-        response_data += data
-
-    # Separa o cabeçalho da resposta dos dados JSON
-    response_json = json.loads(response_data.split(b'\r\n\r\n')[1].decode('utf-8'))
-    # Obtém a lista de IPs do JSON da resposta
-    ips_clientes = response_json.get('client_ip')
-    if ips:
-        print("Lista de IPs dos clientes:")
-        for cliente_ip in ips:
-            print(cliente_ip)
-    else:
-        print("Não tem IPs cadastrados.")
-
-    client_socket.close()
-
-def update_client_status(client_ip, new_status):
-    payload = {'status': new_status}
-    response = requests.put(f'{server_address}/update_status/{client_ip}', json=payload)
-    if response.status_code == 200:
-        print(f'Successfully updated status for {client_ip}')
-    else:
-        print(f'Failed to update status for {client_ip}')
-
-if __name__ == '__main__':
-    while True:
-        print("1. Get Clients")
-        print("2. Update Client Status")
-        choice = input("Enter your choice: ")
-
-        if choice == '1':
-            clients = get_clients()
-            print("Connected Clients:")
-            for client_ip, client_info in clients.items():
-                print(f"IP: {client_ip}, Status: {client_info['status']}")
-
-        elif choice == '2':
-            client_ip = input("Enter client IP: ")
-            new_status = input("Enter new status (true/false): ")
-            update_client_status(client_ip, new_status)
-
         else:
-            print("Invalid choice")
+            print("Opção inválida.")
+
+if __name__ == "__main__":
+    main()
