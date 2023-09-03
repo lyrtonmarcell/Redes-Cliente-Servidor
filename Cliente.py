f71cf23b21e51063f1422265e6833bd5083e245a
lyrtonmarcell <104734284+lyrtonmarcell@users.noreply.github.com>
2023-09-03 17:26:09 -0300

    Update Cliente.py

--- Cliente.py
+++ Cliente.py
@@ -1,90 +1,67 @@
-import socket
+import http.client
 import json
-import uuid
+import socket
+import random
+
+def send_post_request(host, path, payload):
+    headers = {"Content-Type": "application/json"}
+    payload_json = json.dumps(payload)
+    conn = http.client.HTTPConnection(host)
+    conn.request("POST", path, body=payload_json, headers=headers)
+    response = conn.getresponse()
+    data = response.read().decode()
+    conn.close()
+    return response.status, data
+
+def send_tags_to_server(host, path, nome_caixa, tags):
+    payload = {"nome_caixa": nome_caixa, "tags": tags}
+    status, response = send_post_request(host, path, payload)
+    if status == 200:
+        print(f"Tags enviadas com sucesso para o servidor para o caixa '{nome_caixa}'.")
+    else:
+        print(f"Falha ao enviar as tags para o servidor para o caixa '{nome_caixa}'. Status: {status}, Resposta: {response}")
 
 def generate_unique_ip():
-    # Gera um IP único usando um UUID
-    return str(uuid.uuid4())
-
-def send_post_request(ip):
-    # Cria um socket para a comunicação com o servidor
-    client_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-    # Conecta-se ao endereço IP e porta do servidor
-    client_socket.connect(('127.0.0.1', 8080))
-
-    # Prepara o corpo da solicitação POST em formato JSON
-    request_body = json.dumps({'ip': ip})
-    # Cria a requisição POST com os cabeçalhos necessários
-    request = f"POST /add_ip HTTP/1.1\r\nHost: 127.0.0.1:8080\r\nContent-Length: {len(request_body)}\r\nContent-Type: application/json\r\n\r\n{request_body}"
-    # Envia a requisição para o servidor
-    client_socket.send(request.encode('utf-8'))
-    # Recebe a resposta do servidor
-    response = client_socket.recv(1024)
-    # Exibe a resposta decodificada
-    print(response.decode('utf-8'))
-    # Fecha conexão
-    client_socket.close()
-
-def send_get_request():
-        # Cria um socket para a comunicação com o servidor
-    client_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-    # Conecta-se ao endereço IP e porta do servidor
-    client_socket.connect(('127.0.0.1', 8080))
-    # Cria a requisição GET com os cabeçalhos necessários
-    request = "GET /get_ips HTTP/1.1\r\nHost: 127.0.0.1:8080\r\n\r\n"
-    # Envia a requisição para o servidor
-    client_socket.send(request.encode('utf-8'))
-
-    # Inicializa uma variável para armazenar a resposta do servidor
-    response_data = b""
-    # Loop para receber os dados da resposta em blocos de 1024 bytes
-    while True:
-        data = client_socket.recv(1024)
-        if not data:
-            break
-        response_data += data
-
-    # Separa o cabeçalho da resposta dos dados JSON
-    response_json = json.loads(response_data.split(b'\r\n\r\n')[1].decode('utf-8'))
-    # Obtém a lista de IPs do JSON da resposta
-    ips = response_json.get('ips')
-    if ips:
-        print("Lista de IPs:")
-        for ip in ips:
-            print(ip)
-    else:
-        print("Não tem IPs cadastrados.")
-
-    client_socket.close()
+    # Gerar um IP aleatório dentro da faixa 192.168.1.x
+    ip = f"192.168.1.{random.randint(1, 255)}"
+    return ip
 
 def main():
-    client_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-    client_socket.connect(('127.0.0.1', 8080))
+    host = "127.0.0.1:3000"  # Endereço do servidor RESTful
+    unique_ip = generate_unique_ip()
+    caixa_name = f"caixa{unique_ip.replace('.', '')}"
+    print(f"Nome da caixa gerado: {caixa_name}")
 
-    # Gera um IP único e cria a requisição JSON
-    client_ip = generate_unique_ip()
-    client_data = {'client_id': client_ip, 'status': True}
-    client_request = f"POST /add_client HTTP/1.1\r\nContent-Length: {len(json.dumps(client_data))}\r\n\r\n{json.dumps(client_data)}"
+    payload = {"nome": caixa_name, "endereco": unique_ip, "status": "aberto"}
+    status, response = send_post_request(host, "/registrar", payload)
+    if status == 200:
+        print(f"Nome da caixa ({caixa_name}), endereço IP ({unique_ip}) e status 'aberto' enviados com sucesso para o servidor.")
+    else:
+        print(f"Falha ao enviar os dados para o servidor. Status: {status}, Resposta: {response}")
 
-    # Envia a requisição para o servidor
-    client_socket.send(client_request.encode('utf-8'))
-    response = client_socket.recv(1024).decode('utf-8')
-    print(response)
-
-    client_socket.close()
-
-
-if __name__ == '__main__':
-    main()
     while True:
-        action = input("Escolha uma das opções (post/get/exit): ").lower()
-        if action == 'exit':
+        user_input = input("Pressione 'S' para enviar tags ou 'Q' para sair: ").strip().lower()
+        
+        if user_input == 's':
+            tags = [
+                'E20000172211009418905449',
+                'E2000017221101321890548C',
+                'E2000017221101241890547C',
+                'E20000172211010218905459',
+                'E20000172211011118905471',
+                'E20000172211012518905484',
+                'E2000017221100961890544A',
+                'E20000172211011718905474',
+                'E20000172211010118905454'
+            ]
+            send_tags_to_server(host, "/armazenar_tags", caixa_name, tags)
+        elif user_input == 'q':
             break
-        if action == 'post':
-            ip = input("Adicione um IP: ")
-            send_post_request(ip)
-        if action == 'get':
-            send_get_request()
+        else:
+            print("Comando inválido. Pressione 'S' para enviar tags ou 'Q' para sair.")
+
+if __name__ == "__main__":
+    main()
         if action == 'delete':
             ip = input("Exclua um IP existente: ")
             send_delete_request(ip)
