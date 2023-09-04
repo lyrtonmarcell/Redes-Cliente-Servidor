293ae1fc6854d5d16b9a2089178d9c4780adf0a8
lyrtonmarcell <104734284+lyrtonmarcell@users.noreply.github.com>
2023-09-03 23:55:58 -0300

    Update Cliente.py

--- Cliente.py
+++ Cliente.py
@@ -21,10 +21,19 @@ def send_tags_to_server(host, path, nome_caixa, tags):
     else:
         print(f"Falha ao enviar as tags para o servidor para o caixa '{nome_caixa}'. Status: {status}, Resposta: {response}")
 
+
 def generate_unique_ip():
     # Gerar um IP aleatório dentro da faixa 192.168.1.x
     ip = f"192.168.1.{random.randint(1, 255)}"
     return ip
+
+def get_stock_from_server(host, path):
+    conn = http.client.HTTPConnection(host)
+    conn.request("GET", path)
+    response = conn.getresponse()
+    data = response.read().decode()
+    conn.close()
+    return json.loads(data)
 
 def main():
     host = "127.0.0.1:3000"  # Endereço do servidor RESTful
@@ -45,6 +54,8 @@ def main():
         if user_input == 's':
             tags = [
                 'E20000172211009418905449',
+                'E20000172211009418905449',
+                'E2000017221101321890548C',
                 'E2000017221101321890548C',
                 'E2000017221101241890547C',
                 'E20000172211010218905459',
@@ -55,6 +66,27 @@ def main():
                 'E20000172211010118905454'
             ]
             send_tags_to_server(host, "/armazenar_tags", caixa_name, tags)
+            stock = get_stock_from_server(host, "/estoque")
+            print("Produtos armazenados:")
+            
+            # Contagem de produtos com base no número de tags iguais
+            product_count = {}
+            for tag in tags:
+                if tag in stock:
+                    product_name = stock[tag]['nome']
+                    product_price = stock[tag]['preco']
+                    if product_name not in product_count:
+                        product_count[product_name] = {'preco': product_price, 'quantidade': 1}
+                    else:
+                        product_count[product_name]['quantidade'] += 1
+                        product_price=product_price*product_count[product_name]['quantidade']
+
+            total_price = 0
+            for product_name, product_info in product_count.items():
+                print(f"Nome do Produto: {product_name}, Preço: {product_info['preco']}, Quantidade: {product_info['quantidade']}")
+                total_price += product_info['preco'] * product_info['quantidade']
+
+            print(f"Preço Total: {total_price}")
         elif user_input == 'q':
             break
         else:
@@ -62,8 +94,3 @@ def main():
 
 if __name__ == "__main__":
     main()
-        if action == 'delete':
-            ip = input("Exclua um IP existente: ")
-            send_delete_request(ip)
-        if action == 'get':
-            send_get_request()
